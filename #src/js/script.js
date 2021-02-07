837634300d9f308bea3671ac7be43c945068af5c
vetal1117 <lysenkovetal@gmail.com>
2021-02-07 17:47:58 +0200

    подверстал+++

--- #src/js/script.js
+++ #src/js/script.js
@@ -15,16 +15,3 @@ $(document).ready(function() {
 
 
 
-
-
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
-});$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
-	});
-});
